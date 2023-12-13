08c9e88571c855fe4d024adde8264269f0f1f63d
Richard Jean-Baptiste <Richinbk1@gmail.com>
2023-12-13 08:58:18 -0500

    added some styling/ reset function to Settings component

--- src/app/components/THomepage/Settings.js
+++ src/app/components/THomepage/Settings.js
@@ -59,17 +59,29 @@ export default function Settings(props){
     });
   }
 
+  const Reset = () => {
+    SetName(props.profInfo[0]["Name"]);
+    SetAge(props.profInfo[0]["Age"]);
+    SetBio(props.profInfo[0]["Bio"]);
+    SetEdu(props.profInfo[0]["Education"]);
+    SetSpc(props.profInfo[0]["Speciality"]);
+    SetYearsWorking(props.profInfo[0]["YearsWorking"]);
+  }
+
   return (
     <Box>
         <h2>Settings</h2>
-        <Box sx={{ display: 'flex', flexDirection: 'column'}}>
-          <TextField id="outlined-basic" label="Name" variant="outlined" value={name} onChange={handleName}/>
-          <TextField sx={{marginTop: '2%'}} id="outlined-basic" label="Age" variant="outlined" value={age} onChange={handleAge}/>
-          <TextField sx={{marginTop: '2%'}} id="outlined-basic" label="Bio" variant="outlined" value={bio} onChange={handleBio}/>
-          <TextField sx={{marginTop: '2%'}} id="outlined-basic" label="Education" variant="outlined" value={edu} onChange={handleEdu}/>
-          <TextField sx={{marginTop: '2%'}} id="outlined-basic" label="Speciality" variant="outlined" value={spc} onChange={handleSpc}/>
-          <TextField sx={{marginTop: '2%'}} id="outlined-basic" label="Years Working" variant="outlined" value={yearsWorking} onChange={handleYears}/>
-          <Button variant='text' onClick={SaveEdits}>Save</Button>
+        <Box sx={{ display: 'flex', flexDirection: 'column', marginLeft: '3%'}}>
+          <TextField sx={{ width: '24em'}} id="outlined-basic" label="Name" variant="outlined" value={name} onChange={handleName}/>
+          <TextField sx={{marginTop: '2%', width: '24em'}} id="outlined-basic" label="Age" variant="outlined" value={age} onChange={handleAge}/>
+          <TextField sx={{marginTop: '2%', width: '24em'}} id="outlined-basic" label="Bio" variant="outlined" value={bio} onChange={handleBio}/>
+          <TextField sx={{marginTop: '2%', width: '24em'}} id="outlined-basic" label="Education" variant="outlined" value={edu} onChange={handleEdu}/>
+          <TextField sx={{marginTop: '2%', width: '24em'}} id="outlined-basic" label="Speciality" variant="outlined" value={spc} onChange={handleSpc}/>
+          <TextField sx={{marginTop: '2%', width: '24em'}} id="outlined-basic" label="Years Working" variant="outlined" value={yearsWorking} onChange={handleYears}/>
+          <Box sx={{ display: 'flex', flexDirection: 'row'}}>
+            <Button variant='text' sx={{width: '12em'}} onClick={Reset}>Reset</Button>
+            <Button variant='text' sx={{width: '12em'}} onClick={SaveEdits}>Save</Button>
+          </Box>
         </Box> 
     </Box>
   )
